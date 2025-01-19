d16e2157c4ddc773f9f5c1b6cf7bfa0d51877279
Victor <soujvnunes@gmail.com>
2025-01-19 15:42:36 -0300

    feat(page/app) initialize canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 // TODO: remove directive after component client ui
 'use client'
 
+import { Canvas } from 'fabric'
 import NextImage from 'next/image'
-import { useState } from 'react'
+import { useEffect, useId, useRef, useState } from 'react'
 
 export default function Home() {
-  const [url, setUrl] = useState('')
+  const canvasRef = useRef<Canvas>(null)
+  const [imageSrc, setImageSrc] = useState('')
+  const canvasId = useId()
+
+  useEffect(() => {
+    const canvas = new Canvas(canvasId, {
+      width: window.innerWidth,
+      // 64 refers to toolbar's height
+      height: window.innerHeight - 64,
+    })
+
+    canvasRef.current = canvas
+  }, [])
 
   return (
     <main>
-      {url}
+      {imageSrc}
+      <canvas
+        className="w-full h-full bg-neutral-900"
+        id={canvasId}
+      />
       <input
         type="file"
         accept="image/*"
@@ -22,19 +39,19 @@ export default function Home() {
             reader.readAsDataURL(file)
             reader.onload = () => {
               if (typeof reader.result === 'string') {
-                setUrl(reader.result)
+                setImageSrc(reader.result)
               }
             }
           }
         }}
       />
-      {!!url && (
+      {!!imageSrc && (
         <div className="relative h-24">
           <NextImage
             fill
             className="object-cover"
             alt="Preview"
-            src={url}
+            src={imageSrc}
           />
         </div>
       )}
