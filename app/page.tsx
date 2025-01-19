7aaefc1d80febb3ceda8009cce54e288bf89f45c
Victor <soujvnunes@gmail.com>
2025-01-19 14:03:50 -0300

    feat(app/page) add basic image upload/preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,43 @@
-import Image from 'next/image'
-import Link from 'next/link'
+// TODO: remove directive after component client ui
+'use client'
+
+import NextImage from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
-  return <main>Welcome to AINotator.</main>
+  const [url, setUrl] = useState('')
+
+  return (
+    <main>
+      {url}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(event) => {
+          const file = event.target.files?.item(0)
+
+          if (file) {
+            const reader = new FileReader()
+
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+              if (typeof reader.result === 'string') {
+                setUrl(reader.result)
+              }
+            }
+          }
+        }}
+      />
+      {!!url && (
+        <div className="relative h-24">
+          <NextImage
+            fill
+            className="object-cover"
+            alt="Preview"
+            src={url}
+          />
+        </div>
+      )}
+    </main>
+  )
 }
