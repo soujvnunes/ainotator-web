5e42e4d793d60a944d88dd377a6a693e94da10fc
Victor <soujvnunes@gmail.com>
2025-01-19 17:02:02 -0300

    feat(app/page) conditionally show file input if canvas has image

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@
 'use client'
 
 import { Canvas, FabricImage } from 'fabric'
-import { useCallback, useEffect, useId, useRef } from 'react'
+import { useCallback, useId, useRef, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const TOOLBAR_Y = 64
 
 export default function Home() {
   const canvasRef = useRef<Canvas>(null)
   const canvasId = useId()
+  const [currentFile, setCurrentFile] = useState<File | null>(null)
   const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const file = event.target.files?.item(0)
@@ -27,28 +31,48 @@ export default function Home() {
         }
       }
       reader.readAsDataURL(file)
+      setCurrentFile(file)
     },
     [],
   )
-
-  useEffect(() => {
-    const canvas = new Canvas(canvasId, {
+  const handleCanvas = useCallback((node: HTMLCanvasElement) => {
+    const canvas = new Canvas(node.id, {
       width: window.innerWidth,
       // TODO: get 64 (refers to toolbar's height) dynamically
-      height: window.innerHeight - 64,
+      height: window.innerHeight - TOOLBAR_Y,
     })
 
     canvasRef.current = canvas
+
+    return () => {
+      canvas.dispose()
+    }
   }, [])
 
   return (
     <main>
-      <canvas id={canvasId} />
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleFileChange}
-      />
+      <div className="relative bg-neutral-900">
+        <label
+          className={twMerge(
+            'absolute flex w-full h-full',
+            !currentFile && 'z-10',
+            !!currentFile && 'opacity-0',
+          )}>
+          <span className="m-auto">Add a file</span>
+          <input
+            type="file"
+            accept="image/*"
+            className="sr-only"
+            disabled={!!currentFile}
+            onChange={handleFileChange}
+          />
+        </label>
+        <canvas
+          id={canvasId}
+          ref={handleCanvas}
+        />
+      </div>
+      <div style={{ height: TOOLBAR_Y }}>tools</div>
     </main>
   )
 }
