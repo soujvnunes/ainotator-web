a1b282b704b0f4ed2d38eb05348a811d90c212f7
Victor <soujvnunes@gmail.com>
2025-01-21 09:05:35 -0300

    feat(stores/annotator) create slice

--- stores/annotator.ts
+++ stores/annotator.ts
@@ -1,30 +1,21 @@
-import { createStore } from 'zustand'
+import { createSlice } from '@reduxjs/toolkit'
+import { type PayloadAction } from '@reduxjs/toolkit'
 
 interface AnnotatorState {
   file: boolean
 }
 
-interface AnnotatorActions {
-  setFile(file: boolean): void
-}
-
-export type AnnotatorStore = AnnotatorState & AnnotatorActions
-
-export const defaultAnnotator: AnnotatorState = {
+const initialState: AnnotatorState = {
   file: false,
 }
 
-export function createAnnotatorStore(
-  initState: AnnotatorState = defaultAnnotator,
-) {
-  return createStore<AnnotatorStore>()((set) => ({
-    ...initState,
-    setFile: (file) => set(() => ({ file })),
-  }))
-}
-
-export default function annotator() {
-  return {
-    file: false,
-  }
-}
+export default createSlice({
+  name: 'annotator',
+  initialState,
+  reducers: {
+    setFile: (state, action: PayloadAction<AnnotatorState['file']>) => ({
+      ...state,
+      file: action.payload,
+    }),
+  },
+})
