adbd09d542f8dddcfc95167b21d10820555ca295
Victor <soujvnunes@gmail.com>
2025-01-21 10:25:13 -0300

    feat(containers/Annotator) extract toolbar modes

---
+++ containers/Annotator/AnnotatorToolbarModes.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useAnnotatorRefs } from '@/providers/AnnotatorRefsProvider'
+import { PencilBrush } from 'fabric'
+import { useCallback, useState } from 'react'
+import { useAnnotatorState } from '@/providers/AnnotatorProvider'
+
+const annotatorToolbarModes = ['brush', 'polygon'] as const
+
+export type Modes = (typeof annotatorToolbarModes)[number] | null
+
+export default function AnnotatorToolbarModes() {
+  const annotatorRefs = useAnnotatorRefs()
+  const file = useAnnotatorState((state) => state.annotator.file)
+  const [mode, setMode] = useState<Modes>(null)
+  const handleBrush = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      const mode = event.currentTarget.name as Modes
+
+      setMode((prevMode) => {
+        const canvas = annotatorRefs.canvas.current
+        const newMode = prevMode === mode ? null : mode
+
+        if (canvas != null) {
+          canvas.isDrawingMode = !!newMode
+
+          if (newMode === 'brush') {
+            canvas.freeDrawingBrush = new PencilBrush(canvas)
+            canvas.freeDrawingBrush.color = 'rgba(255,0,0,0.5)'
+            canvas.freeDrawingBrush.width = 20
+          }
+        }
+
+        return newMode
+      })
+    },
+    [],
+  )
+
+  return (
+    <div className="space-x-4">
+      {annotatorToolbarModes.map((annotatorToolbarMode) => (
+        <button
+          type="button"
+          className="disabled:text-white/60"
+          key={annotatorToolbarMode}
+          name={annotatorToolbarMode}
+          onClick={handleBrush}
+          disabled={!file || (!!mode && mode !== annotatorToolbarMode)}>
+          {annotatorToolbarMode}
+        </button>
+      ))}
+    </div>
+  )
+}
--- containers/Annotator/annotatorToolbarModes.ts
+++
@@ -1,5 +0,0 @@
-const annotatorToolbarModes = ['brush', 'polygon'] as const
-
-export type Modes = (typeof annotatorToolbarModes)[number] | null
-
-export default annotatorToolbarModes
