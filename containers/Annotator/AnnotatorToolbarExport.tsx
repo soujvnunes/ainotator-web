929159cc6d654927f1471a4958a6124208a50c5d
Victor <soujvnunes@gmail.com>
2025-01-21 16:07:41 -0300

    feat(containers/AnnotatorToolbarExport) move form actions to form container

--- containers/Annotator/AnnotatorToolbarExport.tsx
+++ containers/Annotator/AnnotatorToolbarExport.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Link from 'next/link'
 import {
   Button,
   CloseButton,
@@ -10,7 +9,7 @@ import {
   DialogTitle,
 } from '@headlessui/react'
 import { useState } from 'react'
-import AnnotatorToolbarExportLicenseForm from './AnnotatorToolbarExportLicenseForm'
+import AnnotatorToolbarExportForm from './AnnotatorToolbarExportForm'
 import { useAnnotatorState } from '@/providers/AnnotatorProvider'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
@@ -36,34 +35,21 @@ export default function AnnotatorToolbarExport() {
       </Button>
       <Dialog
         open={open}
-        as="div"
         className="relative z-10 focus:outline-none"
         onClose={handleClose}>
         <DialogBackdrop className="fixed inset-0 bg-black/80 backdrop-blur-lg" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex items-center justify-center min-h-full p-4">
             <DialogPanel
-              as="form"
               transition
               className="w-full max-w-md bg-neutral-900 duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0">
-              <DialogTitle className="flex">
-                <span className="sr-only">Licenses</span>
-                <CloseButton className="inline-flex w-10 h-10 ml-auto hover:bg-white/10">
+              <DialogTitle className="flex items-center pl-4 text-xs font-medium tracking-wider uppercase">
+                Details
+                <CloseButton className="inline-flex w-10 h-10 ml-auto hover:bg-white/5">
                   <XMarkIcon className="m-auto size-6" />
                 </CloseButton>
               </DialogTitle>
-              <AnnotatorToolbarExportLicenseForm />
-              <div className="flex items-center mt-4">
-                <Button className="inline-flex items-center justify-center w-full px-4 text-xs uppercase font-semibold tracking-wider h-10	 text-white  data-[hover]:bg-white/10 ">
-                  Validate
-                </Button>
-                <Link
-                  className="inline-flex data-[disabled]:text-white/40 data-[disabled]:bg-white/10 data-[disabled]:pointer-events-none items-center justify-center w-full px-4 text-xs uppercase font-semibold tracking-wider h-10	 text-white  data-[hover]:bg-white/10 "
-                  href=""
-                  data-disabled>
-                  Export
-                </Link>
-              </div>
+              <AnnotatorToolbarExportForm />
             </DialogPanel>
           </div>
         </div>
