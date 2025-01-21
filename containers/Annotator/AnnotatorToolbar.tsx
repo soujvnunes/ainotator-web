095a7530f032747dc640c5feac43fc7783388a0d
Victor <soujvnunes@gmail.com>
2025-01-20 21:21:24 -0300

    feat(containers/Annotator) compose annotator toolbar

---
+++ containers/Annotator/AnnotatorToolbar.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useAnnotatorRefs } from '@/providers/AnnotatorRefsProvider'
+import { PencilBrush } from 'fabric'
+import { useCallback, useState } from 'react'
+import annotatorToolbarModes, { type Modes } from './annotatorToolbarModes'
+import Link from 'next/link'
+import { useAnnotator } from '@/providers/AnnotatorProvider'
+
+export default function AnnotatorToolbar() {
+  const annotator = useAnnotatorRefs()
+  const file = useAnnotator((state) => state.file)
+  const [mode, setMode] = useState<Modes>(null)
+  const handleBrush = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      const mode = event.currentTarget.name as Modes
+
+      setMode((prevMode) => {
+        const canvas = annotator.canvas.current
+        const newMode = prevMode === mode ? null : mode
+
+        if (canvas != null) {
+          canvas.isDrawingMode = !!newMode
+
+          if (newMode === 'brush') {
+            canvas.freeDrawingBrush = new PencilBrush(canvas)
+            canvas.freeDrawingBrush.color = 'rgba(255,0,0,0.5)'
+            canvas.freeDrawingBrush.width = 20
+          }
+        }
+
+        return newMode
+      })
+    },
+    [],
+  )
+
+  return (
+    <div className="flex items-center h-16 px-4">
+      <div className="space-x-4">
+        {annotatorToolbarModes.map((annotatorToolbarMode) => (
+          <button
+            type="button"
+            className="disabled:text-white/60"
+            key={annotatorToolbarMode}
+            name={annotatorToolbarMode}
+            onClick={handleBrush}
+            disabled={!file || (!!mode && mode !== annotatorToolbarMode)}>
+            {annotatorToolbarMode}
+          </button>
+        ))}
+      </div>
+      <Link
+        className="ml-auto data-[disabled]:text-white/60 data-[disabled]:pointer-events-none"
+        href=""
+        data-disabled>
+        export
+      </Link>
+    </div>
+  )
+}
