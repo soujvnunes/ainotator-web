1041a2f007afc436b982418fdb708170c2b1889e
Victor <soujvnunes@gmail.com>
2025-01-22 18:09:58 -0300

    feat(containers/AnnotatorToolbar) implement AnnotatorToolbarAdd

--- containers/Annotator/AnnotatorToolbar.tsx
+++ containers/Annotator/AnnotatorToolbar.tsx
@@ -1,3 +1,4 @@
+import AnnotatorToolbarAdd from './AnnotatorToolbarAdd'
 import AnnotatorToolbarExport from './AnnotatorToolbarExport'
 import AnnotatorToolbarModes from './AnnotatorToolbarModes'
 import { TOOLBAR_Y } from './annotatorToolbar.utils'
@@ -6,7 +7,8 @@ export default function AnnotatorToolbar() {
   return (
     <div
       style={{ height: TOOLBAR_Y }}
-      className="flex items-center justify-between px-4">
+      className="flex items-center justify-between">
+      <AnnotatorToolbarAdd />
       <AnnotatorToolbarModes />
       <AnnotatorToolbarExport />
     </div>
