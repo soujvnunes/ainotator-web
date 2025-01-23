05ad484530fa40e55c6c64161d5c7fe2712229ca
Victor <soujvnunes@gmail.com>
2025-01-23 19:49:05 -0300

    feat(ui/Dialog) extract

---
+++ ui/Dialog.tsx
@@ -0,0 +1,66 @@
+import ToolbarAddForm from '@/containers/Toolbar/ToolbarAddForm'
+import {
+  Button,
+  CloseButton,
+  Dialog as HeadlessDialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  ButtonProps,
+} from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import React, { useCallback, useState } from 'react'
+
+interface DialogProps {
+  title: string
+  buttonProps?: ButtonProps<'button'>
+}
+
+export default function Dialog({
+  title,
+  children,
+  buttonProps,
+}: React.PropsWithChildren<DialogProps>) {
+  const [open, setOpen] = useState(false)
+  const handleOpen = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      setOpen(true)
+      buttonProps?.onClick?.(event)
+    },
+    [buttonProps?.onClick],
+  )
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [])
+
+  return (
+    <>
+      <Button
+        className="h-16 w-16 flex-shrink-0 inline-flex bg-white py-2 px-4 text-sm font-medium text-black disabled:cursor-not-allowed focus:outline-none data-[hover]:bg-white/60 disabled:bg-white/60 disabled:text-black/60"
+        {...buttonProps}
+        onClick={handleOpen}
+      />
+      <HeadlessDialog
+        className="relative z-10 focus:outline-none"
+        open={open}
+        onClose={handleClose}>
+        <DialogBackdrop className="fixed inset-0 bg-black/80 backdrop-blur-lg" />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex items-center justify-center min-h-full p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0">
+              <DialogTitle className="flex items-center pl-4 text-xs font-medium tracking-wider uppercase bg-neutral-800">
+                {title}
+                <CloseButton className="inline-flex w-10 h-10 ml-auto hover:bg-white/5">
+                  <XMarkIcon className="m-auto size-6" />
+                </CloseButton>
+              </DialogTitle>
+              {children}
+            </DialogPanel>
+          </div>
+        </div>
+      </HeadlessDialog>
+    </>
+  )
+}
