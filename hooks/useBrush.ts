f7c9eca7c75ca2cf571e0440736c8e26a691efd6
Victor <soujvnunes@gmail.com>
2025-01-23 14:09:31 -0300

    feat(hooks/useBrush) disable selection

--- hooks/useBrush.ts
+++ hooks/useBrush.ts
@@ -12,16 +12,11 @@ export default function useBrush() {
   useEffect(() => {
     const canvas = annotatorRefs.canvas.current
 
-    if (canvas == null) return
-
-    canvas.isDrawingMode = false
-
-    if (category?.type !== 'brush') return
+    if (canvas == null || category?.type !== 'brush') return
 
     canvas.isDrawingMode = true
     canvas.freeDrawingBrush = new PencilBrush(canvas)
     canvas.freeDrawingBrush.color = `rgb(${category.color} / 0.4)`
-    // TODO: size
     canvas.freeDrawingBrush.width = 20
-  })
+  }, [annotatorRefs, category])
 }
