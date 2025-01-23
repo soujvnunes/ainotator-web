6e81f1815ffa89d1f8d6c6f37fa93c3bdbc5ddba
Victor <soujvnunes@gmail.com>
2025-01-23 13:33:26 -0300

    feat(containers/AnnotatorToolbarAdd)  implement add categories added preview container

--- containers/Annotator/AnnotatorToolbarAdd.tsx
+++ containers/Annotator/AnnotatorToolbarAdd.tsx
@@ -12,6 +12,7 @@ import { useCallback, useState } from 'react'
 import AnnotatorToolbarAddForm from './AnnotatorToolbarAddForm'
 import { PlusIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { useAnnotatorState } from '@/providers/AnnotatorProvider'
+import AnnotatorToolbarAddedCategories from './AnnotatorToolbarAddedCategories'
 
 export default function AnnotatorToolbarAdd() {
   const mode = useAnnotatorState((state) => state.annotator.current.mode)
@@ -26,21 +27,21 @@ export default function AnnotatorToolbarAdd() {
   return (
     <>
       <Button
+        className="h-16 w-16 flex-shrink-0 inline-flex bg-white py-2 px-4 text-sm font-medium text-black disabled:cursor-not-allowed focus:outline-none data-[hover]:bg-white/60 disabled:bg-white/60 disabled:text-black/60"
         onClick={handleOpen}
-        disabled={!['editting', 'annotating'].includes(mode)}
-        className="h-16 w-16 flex-shrink-0 inline-flex bg-white py-2 px-4 text-sm font-medium text-black disabled:cursor-not-allowed focus:outline-none data-[hover]:bg-white/60 disabled:bg-white/60 disabled:text-black/60">
+        disabled={!['editting', 'annotating'].includes(mode)}>
         <PlusIcon className="m-auto size-6" />
       </Button>
       <Dialog
-        open={open}
         className="relative z-10 focus:outline-none"
+        open={open}
         onClose={handleClose}>
         <DialogBackdrop className="fixed inset-0 bg-black/80 backdrop-blur-lg" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex items-center justify-center min-h-full p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md bg-neutral-900 duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0">
+              className="w-full max-w-md duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0">
               <DialogTitle className="flex items-center pl-4 text-xs font-medium tracking-wider uppercase bg-neutral-800">
                 Annotation Class
                 <CloseButton className="inline-flex w-10 h-10 ml-auto hover:bg-white/5">
@@ -48,6 +49,7 @@ export default function AnnotatorToolbarAdd() {
                 </CloseButton>
               </DialogTitle>
               <AnnotatorToolbarAddForm />
+              <AnnotatorToolbarAddedCategories />
             </DialogPanel>
           </div>
         </div>
