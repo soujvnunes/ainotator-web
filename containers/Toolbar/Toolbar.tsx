4ebf8f972e6dd346c03d1a660b70af888cafdf24
Victor <soujvnunes@gmail.com>
2025-01-23 20:35:51 -0300

    feat(containers/Toolbar) remove export

--- containers/Toolbar/Toolbar.tsx
+++ containers/Toolbar/Toolbar.tsx
@@ -1,13 +1,11 @@
 import ToolbarAdd from './ToolbarAdd'
 import ToolbarCategories from './ToolbarCategories'
-import ToolbarExport from './ToolbarExport'
 
 export default function Toolbar() {
   return (
     <div className="flex items-center h-16">
       <ToolbarAdd />
       <ToolbarCategories />
-      <ToolbarExport />
     </div>
   )
 }
