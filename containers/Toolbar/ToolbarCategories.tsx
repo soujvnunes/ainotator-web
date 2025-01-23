ce29e609aa248806dcffa39625593c7a93357f8c
Victor <soujvnunes@gmail.com>
2025-01-23 20:36:33 -0300

    feat(containers/ToolbarCategories) allow items to overflow

--- containers/Toolbar/ToolbarCategories.tsx
+++ containers/Toolbar/ToolbarCategories.tsx
@@ -7,7 +7,7 @@ export default function ToolbarCategories() {
   const categories = useAppState((state) => state.annotator.added.categories)
 
   return (
-    <ul className="flex w-full">
+    <ul className="flex w-full overflow-y-hidden">
       {categories.map((category) => (
         <li key={category.id}>
           <ToolbarCategoriesItem {...category} />
