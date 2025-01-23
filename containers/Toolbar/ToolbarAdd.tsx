7d236260f1fe77d2a577aa21acd197c437616c71
Victor <soujvnunes@gmail.com>
2025-01-23 20:36:15 -0300

    feat(containers/ToolbarAdd) adjust a11y

--- containers/Toolbar/ToolbarAdd.tsx
+++ containers/Toolbar/ToolbarAdd.tsx
@@ -13,6 +13,9 @@ export default function ToolbarAdd() {
     <Dialog
       title="Annotation Class"
       buttonProps={{
+        'aria-label': 'Add categories to start annotating the image',
+        className:
+          'h-16 w-16 flex-shrink-0 inline-flex bg-white py-2 px-4 text-sm font-medium text-black disabled:cursor-not-allowed focus:outline-none data-[hover]:bg-white/60 disabled:bg-white/60 disabled:text-black/60',
         disabled: !['editting', 'annotating'].includes(mode),
         children: <PlusIcon className="m-auto size-6" />,
       }}>
