20bb4c58fc19cc85eeeb50e79f5dd84e13c7da6f
Victor <soujvnunes@gmail.com>
2025-01-22 21:35:22 -0300

    feat(containers/AnnotatorCanvas) adjust brush mode type

--- containers/Annotator/AnnotatorCanvas.tsx
+++ containers/Annotator/AnnotatorCanvas.tsx
@@ -1,42 +1,73 @@
 'use client'
 
 import { useAnnotatorRefs } from '@/providers/AnnotatorRefsProvider'
-import { Canvas } from 'fabric'
-import { useCallback } from 'react'
+import { Canvas, PencilBrush } from 'fabric'
+import { useEffect, useId } from 'react'
 import AnnotatorCanvasUploader from './AnnotatorCanvasUploader'
 import { twMerge } from 'tailwind-merge'
-import { useAnnotatorState } from '@/providers/AnnotatorProvider'
+import {
+  useAnnotatorDispatch,
+  useAnnotatorState,
+} from '@/providers/AnnotatorProvider'
 import { TOOLBAR_Y } from './annotatorToolbar.utils'
+import getDatasetAnnotation from '@/helpers/getDatasetAnnotation'
 
 export default function AnnotatorCanvas() {
   const annotatorRefs = useAnnotatorRefs()
-  const name = useAnnotatorState((state) => state.annotator.mode.name)
-  const handleCanvas = useCallback((node: HTMLCanvasElement) => {
-    const canvas = new Canvas(node.id, {
+  const dispatch = useAnnotatorDispatch()
+  const mode = useAnnotatorState((state) => state.annotator.mode)
+  const canvasId = useId()
+
+  useEffect(() => {
+    annotatorRefs.canvas.current = new Canvas(canvasId, {
       width: window.innerWidth,
       height: window.innerHeight - TOOLBAR_Y,
     })
 
-    annotatorRefs.canvas.current = canvas
-
     return () => {
-      canvas.dispose()
-      annotatorRefs.canvas.current = null
+      annotatorRefs.canvas.current?.dispose()
     }
-  }, [])
+  }, [canvasId])
+  useEffect(() => {
+    const canvas = annotatorRefs.canvas.current
+
+    if (canvas == null) return
+
+    if (mode.name !== 'annotating') {
+      const datasetAnnotation = getDatasetAnnotation(canvas, {
+        isCrowded: false,
+        id: {
+          image: 0, // TODO: dynamically when image is set
+          category: 0, // TODO: dynamically when class is set
+          annotation: 0, // TODO: dynamically when mouse is up
+        },
+      })
+
+      if (datasetAnnotation) dispatch.dataset.addAnnotation(datasetAnnotation)
+
+      canvas.isDrawingMode = false
+    } else {
+      if (mode.category.type === 'brush') {
+        canvas.isDrawingMode = true
+        canvas.freeDrawingBrush = new PencilBrush(canvas)
+        canvas.freeDrawingBrush.color = `rgb(${mode.category.color} / 0.4)`
+        // TODO: size
+        canvas.freeDrawingBrush.width = 20
+      } else if (mode.category.type === 'polygon') {
+        // TODO: polygon
+      }
+    }
+  })
 
   return (
     <div
       style={{ height: `calc(100vh - ${TOOLBAR_Y}px)` }}
       className={twMerge(
         'relative bg-neutral-900 transition-[background-color]',
-        name === 'waiting' && 'hover:bg-neutral-900/60',
+        mode.name === 'waiting' && 'hover:bg-neutral-900/60',
       )}>
       <AnnotatorCanvasUploader />
-      <canvas
-        id="ainotator"
-        ref={handleCanvas}
-      />
+      <canvas id={canvasId} />
     </div>
   )
 }
