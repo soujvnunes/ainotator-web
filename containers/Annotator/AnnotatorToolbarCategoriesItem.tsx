ed02b58ee13e59cabed288f4bf5a1dd0c8f507f8
Victor <soujvnunes@gmail.com>
2025-01-23 13:34:08 -0300

    feat(containers/AnnotatorToolbarCategoriesItem) add is crowded icon

--- containers/Annotator/AnnotatorToolbarCategoriesItem.tsx
+++ containers/Annotator/AnnotatorToolbarCategoriesItem.tsx
@@ -8,7 +8,11 @@ import {
 import { useAnnotatorRefs } from '@/providers/AnnotatorRefsProvider'
 import type { AnnotatorCategory, AnnotatorCurrent } from '@/stores/annotator'
 import { Button } from '@headlessui/react'
-import { CubeTransparentIcon, PaintBrushIcon } from '@heroicons/react/24/solid'
+import {
+  CubeTransparentIcon,
+  PaintBrushIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/solid'
 import { useCallback } from 'react'
 import { twMerge } from 'tailwind-merge'
 
@@ -37,8 +41,7 @@ export default function AnnotatorToolbarCategoriesItem(
     if (!canvas) return
 
     const datasetAnnotation = getDatasetAnnotation(canvas, {
-      // TODO: add switch on add modal
-      isCrowded: false,
+      isCrowd: props.isCrowd,
       id: {
         image: images[0].id,
         category: props.id,
@@ -55,7 +58,7 @@ export default function AnnotatorToolbarCategoriesItem(
       onClick={handleCategory}
       className={twMerge(
         'cursor-pointer w-24 h-16 px-3 py-2 text-sm border-y-4 font-medium leading-none border-y-[--border-color] tracking-wider text-left uppercase truncate bg-opacity-20 hover:bg-opacity-40',
-        isCurrent ? 'border-b-transparent' : ' border-t-transparent',
+        isCurrent ? 'border-b-transparent' : 'border-t-transparent',
         isDisabled && 'border-y-transparent cursor-not-allowed text-white/60',
       )}
       style={
@@ -70,6 +73,7 @@ export default function AnnotatorToolbarCategoriesItem(
         ) : (
           <PaintBrushIcon className="size-4" />
         )}
+        {props.isCrowd === 'yes' && <UserGroupIcon className="ml-2 size-4" />}
       </span>
       {props.name}
     </Button>
