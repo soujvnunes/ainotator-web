ef182e0a4506d95090df486ed61f9daa69466688
Victor <soujvnunes@gmail.com>
2025-01-25 22:58:56 -0300

    feat(ui/Typography) compose

---
+++ ui/Typography/Typography.styles.ts
@@ -0,0 +1,14 @@
+import { cva } from 'class-variance-authority'
+
+const typography = {
+  root: cva('', {
+    variants: {
+      variant: {
+        label: 'text-xs font-semibold uppercase tracking-widest',
+        caption: 'text-xs font-semibold',
+      },
+    },
+  }),
+}
+
+export default typography
