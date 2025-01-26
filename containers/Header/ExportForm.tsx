30319cb93759d88c92364fbbf27e503dfa575bb3
Victor <soujvnunes@gmail.com>
2025-01-25 21:03:49 -0300

    feat(containers/Header) implement useFormSubmit

--- containers/Header/ExportForm.tsx
+++ containers/Header/ExportForm.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { useCallback, useState, useTransition } from 'react'
+import { useState, useTransition } from 'react'
 
-import validateDataset, {
-  type ValidateDataset,
+import validateDataset from '@/actions/validateDataset'
+import type {
+  Dataset,
+  DatasetInfo,
+  DatasetLicense,
+  ValidateDataset,
 } from '@/actions/validateDataset'
 import useAppDispatch from '@/hooks/useAppDispatch'
 import useAppState from '@/hooks/useAppState'
 import useCanvasRefs from '@/hooks/useCanvasRefs'
+import useFormSubmit from '@/hooks/useFormSubmit'
+import { fields, tabs } from '@/lib/exportForm'
+import generateLink from '@/lib/generateLink'
 import isValidationSuccessful from '@/lib/isValidationSuccessful'
-import { toolbarExportFormFields } from '@/lib/toolbarExportFormFields'
-import { toolbarExportFormState } from '@/lib/toolbarExportFormState'
 import {
   Button,
   Field,
@@ -28,138 +33,132 @@ import {
 import { CheckIcon } from '@heroicons/react/24/solid'
 import { twMerge } from 'tailwind-merge'
 
+interface ExportFormFields {
+  license_name: string
+  license_url: string
+  info_description: string
+  info_url: string
+  info_version: string
+  info_year: string
+  info_contributor: string
+  info_date_created: string
+}
+
 export default function ExportForm() {
   const dispatch = useAppDispatch()
   const annotatorRef = useCanvasRefs()
   const images = useAppState((state) => state.dataset.images)
   const categories = useAppState((state) => state.dataset.categories)
   const annotations = useAppState((state) => state.dataset.annotations)
+  const licenses = useAppState((state) => state.annotator.previous.licenses)
+  const info = useAppState((state) => state.annotator.previous.info)
   const closeToolbarExport = useClose()
   const [isPending, startTransition] = useTransition()
-  const [tabId, setTabId] = useState(0)
   const [validation, setValidation] = useState<ValidateDataset | null>(null)
-  // TODO: add the possibility of filling this with previous info and licenses from state.annotator.
-  const [fields, setFields] = useState(() => toolbarExportFormState)
-  const handleChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const currentTab = !tabId ? 'license' : 'info'
+  const formSubmit = useFormSubmit<ExportFormFields>((fields) => {
+    const newLicense: DatasetLicense = {
+      name: fields.license_name,
+      url: fields.license_url,
+      id: images[0].id,
+    }
+    const newInfo: DatasetInfo = {
+      description: fields.info_description,
+      url: fields.info_url,
+      version: fields.info_version,
+      year: +fields.info_year,
+      contributor: fields.info_contributor,
+      date_created: fields.info_date_created,
+    }
 
-      setFields((prevState) => ({
-        ...prevState,
-        [currentTab]: {
-          ...prevState[currentTab],
-          [event.target.name]: event.target.value,
-        },
-      }))
-    },
-    [tabId],
-  )
-  const handleValidate = useCallback(
-    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      event.preventDefault()
+    dispatch.annotator.setMode('exporting')
+    startTransition(async () => {
+      const newDataset: Dataset = {
+        images,
+        categories,
+        annotations,
+        licenses: [newLicense],
+        info: newInfo,
+      }
+      const validation = await validateDataset(newDataset)
 
-      const newLicense = { ...fields.license, id: images[0].id }
-      const newInfo = fields.info
-
-      dispatch.annotator.setMode('exporting')
-      startTransition(async () => {
-        const newDataset = {
-          images,
-          categories,
-          annotations,
-          licenses: [newLicense],
-          info: newInfo,
+      startTransition(() => {
+        if (!isValidationSuccessful(validation)) {
+          return setValidation(validation)
         }
-        const validation = await validateDataset(newDataset)
 
-        await new Promise((resolve) => setTimeout(resolve, 3000))
-
-        startTransition(() => {
-          setValidation(validation)
-
-          if (!isValidationSuccessful(validation)) return
-
-          // CLEAN LOCAL STATES
-          setValidation(null)
-          // CREATE THE LINK AND DOWNLOAD THE FILE
-          // TODO: get the file by a route probably
-          const blob = new Blob([JSON.stringify(newDataset, null, 2)], {
-            type: 'application/json',
-          })
-          const url = URL.createObjectURL(blob)
-          const link = window.document.createElement('a')
-
-          link.href = url
-          link.download = `${images[0].file_name}_${fields.info.date_created}_annotations.json`
-          window.document.body.appendChild(link)
-          link.click()
-          window.document.body.removeChild(link)
-          URL.revokeObjectURL(url)
-          // DISPATCH VALID DETAILS AND RESET ANNOTATING STATE
-          dispatch.annotator.addLicense(newLicense)
-          dispatch.annotator.setInfo(newInfo)
-          dispatch.annotator.setMode('waiting')
-          // CLEAR REFS
-          annotatorRef.file.current = null
-          annotatorRef.image.current = null
-          annotatorRef.canvas.current?.clear()
-          // CLOSE DIALOG
-          closeToolbarExport()
+        setValidation(null)
+        generateLink({
+          name: `${images[0].file_name}_${fields.info_date_created}_annotations.json`,
+          value: newDataset,
         })
+        dispatch.annotator.addLicense(newLicense)
+        dispatch.annotator.setInfo(newInfo)
+        dispatch.annotator.setMode('waiting')
+        // CLEAR REFS
+        annotatorRef.file.current = null
+        annotatorRef.image.current = null
+        annotatorRef.canvas.current?.clear()
+        // CLOSE DIALOG
+        closeToolbarExport()
       })
-    },
-    [
-      fields.license,
-      fields.info,
-      images,
-      dispatch.annotator,
-      categories,
-      annotations,
-      annotatorRef.file,
-      annotatorRef.image,
-      annotatorRef.canvas,
-      closeToolbarExport,
-    ],
-  )
+    })
+  })
 
   return (
-    <form className="bg-neutral-900">
+    <form
+      className="bg-neutral-900"
+      onSubmit={formSubmit.onSubmit}>
       <Fieldset disabled={isPending}>
         <Legend className="bg-neutral-800 px-4 pb-2 text-white/60">
           Fill in the license and information dataset details to validate it
           before exporting.
         </Legend>
-        <TabGroup
-          selectedIndex={tabId}
-          onChange={setTabId}>
+        <TabGroup>
           <TabList className="flex bg-neutral-800">
-            {toolbarExportFormFields.map((tab) => (
+            {tabs.map((tab) => (
               <Tab
                 key={tab.name}
                 className="inline-flex h-10 w-full items-center justify-center border-b-2 border-b-transparent px-4 text-xs font-semibold uppercase tracking-wider text-white hover:border-gray-50 data-[selected]:border-gray-50/20 data-[hover]:bg-white/5 hover:data-[selected]:border-gray-50">
-                {tab.name}
+                {tab.label}
               </Tab>
             ))}
           </TabList>
           <TabPanels>
-            {toolbarExportFormFields.map((tab) => (
+            {tabs.map((tab) => (
               <TabPanel key={tab.name}>
-                {tab.fields.map((field) => (
-                  <Field
-                    className="mt-4"
-                    key={field.name}>
-                    <Label className="cursor-pointer px-4 text-sm font-medium">
-                      {field.label}
-                    </Label>
-                    <Input
-                      type="text"
-                      name={field.name}
-                      placeholder={field.placeholder}
-                      className="border-x-none border-t-none mt-2 block h-10 w-full border-b-2 border-b-transparent bg-white/5 px-4 text-sm focus:outline-none data-[focus]:border-b-2 data-[focus]:border-gray-50/20"
-                      onChange={handleChange}
-                    />
-                  </Field>
-                ))}
+                {fields
+                  .filter((field) => field.tab === tab.name)
+                  .map((field) => {
+                    let defaultValue: string | number | undefined
+
+                    if (tab.name === 'license' && !!licenses.length) {
+                      // TODO: allow choosing the license/dropdown with previous added licenses? so instead of [license] it's [index-of-the-license]
+                      const [license] = licenses
+
+                      defaultValue = license[field.name as keyof DatasetLicense]
+                    }
+
+                    if (tab.name === 'info' && info != null) {
+                      defaultValue = info[field.name as keyof DatasetInfo]
+                    }
+
+                    return (
+                      <Field
+                        className="mt-4"
+                        key={field.name}>
+                        <Label className="cursor-pointer px-4 text-sm font-medium">
+                          {field.label}
+                        </Label>
+                        <Input
+                          type="text"
+                          className="border-x-none border-t-none mt-2 block h-10 w-full border-b-2 border-b-transparent bg-white/5 px-4 text-sm focus:outline-none data-[focus]:border-b-2 data-[focus]:border-gray-50/20"
+                          name={`${tab.name}_${field.name}`}
+                          placeholder={field.placeholder}
+                          defaultValue={defaultValue}
+                        />
+                      </Field>
+                    )
+                  })}
               </TabPanel>
             ))}
           </TabPanels>
@@ -167,15 +166,11 @@ export default function ExportForm() {
         <div className="flex items-center">
           <Button
             type="submit"
-            onClick={handleValidate}
             className={twMerge(
               'inline-flex h-10 w-full items-center justify-center px-4 text-xs font-semibold uppercase tracking-wider text-white data-[disabled]:cursor-not-allowed data-[hover]:bg-white/5 data-[disabled]:text-white/40',
               isValidationSuccessful(validation) &&
                 'pointer-events-none text-green-400',
-            )}
-            disabled={Object.values(
-              Object.assign({}, ...Object.values(fields)),
-            ).some((value) => !value)}>
+            )}>
             {isValidationSuccessful(validation) ? (
               <>
                 <CheckIcon className="mr-2 size-6" />
