d55fcc0c47c28c6411dcc7f5a9ad42e7044d90fb
Victor <soujvnunes@gmail.com>
2025-01-27 19:12:08 -0300

    feat(ui/Button) implement typography classes

--- ui/Button/styles.ts
+++ ui/Button/styles.ts
@@ -1,23 +1,14 @@
 import { cva } from 'class-variance-authority'
-import { twMerge } from 'tailwind-merge'
-
-import typographyStyles from '../Typography/styles'
 
 const buttonStyles = {
   root: cva(
-    twMerge(
-      typographyStyles.root({
-        variant: 'label',
-        className:
-          'inline-flex h-10 min-w-10 flex-shrink-0 items-center justify-center px-4 text-xs data-[disabled]:cursor-not-allowed data-[focus]:outline-none',
-      }),
-    ),
+    'text-label inline-flex h-10 min-w-10 flex-shrink-0 items-center justify-center px-4 text-xs data-[disabled]:cursor-not-allowed data-[focus]:outline-none',
     {
       variants: {
         variant: {
           filled:
             'bg-white text-black data-[disabled]:bg-white/60 data-[hover]:bg-white/60 data-[disabled]:text-black/60',
-          text: 'text-white data-[disabled]:bg-neutral-800 data-[hover]:bg-white/20 data-[disabled]:text-white/60',
+          text: 'text-white data-[hover]:bg-white/20 data-[disabled]:text-white/40',
         },
         fullWidth: { true: 'w-full', false: '' },
       },
