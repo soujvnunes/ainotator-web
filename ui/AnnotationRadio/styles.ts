a7e2b20f98dcd0661c62d54751b014d562e1bdc1
Victor <soujvnunes@gmail.com>
2025-01-27 16:10:44 -0300

    feat(ui/AnnotationRadio) adjust colors

--- ui/AnnotationRadio/styles.ts
+++ ui/AnnotationRadio/styles.ts
@@ -4,7 +4,7 @@ import typographyStyles from '../Typography/styles'
 
 const annotationRadioStyles = {
   root: cva(
-    'group inline-flex h-16 w-24 cursor-pointer flex-col justify-between truncate',
+    'group inline-flex h-16 w-24 cursor-pointer flex-col justify-between',
     {
       variants: {
         color: {
@@ -44,31 +44,34 @@ const annotationRadioStyles = {
             false: '',
           },
           color: {
-            red: 'group-data-[checked]:text-red-600',
-            orange: 'group-data-[checked]:text-orange-700',
-            amber: 'group-data-[checked]:text-amber-700',
-            yellow: 'group-data-[checked]:text-yellow-700',
-            lime: 'group-data-[checked]:text-lime-700',
-            green: 'group-data-[checked]:text-green-700',
-            emerald: 'group-data-[checked]:text-emerald-700',
-            teal: 'group-data-[checked]:text-teal-700',
-            cyan: 'group-data-[checked]:text-cyan-700',
-            sky: 'group-data-[checked]:text-sky-700',
-            blue: 'group-data-[checked]:text-blue-600',
-            indigo: 'group-data-[checked]:text-indigo-600',
-            violet: 'group-data-[checked]:text-violet-600',
-            purple: 'group-data-[checked]:text-purple-600',
-            fuchsia: 'group-data-[checked]:text-fuchsia-600',
-            pink: 'group-data-[checked]:text-pink-600',
-            rose: 'group-data-[checked]:text-rose-600',
-            neutral: 'group-data-[checked]:text-neutral-600',
+            red: 'group-data-[checked]:text-red-400',
+            orange: 'group-data-[checked]:text-orange-300',
+            amber: 'group-data-[checked]:text-amber-300',
+            yellow: 'group-data-[checked]:text-yellow-500',
+            lime: 'group-data-[checked]:text-lime-300',
+            green: 'group-data-[checked]:text-green-300',
+            emerald: 'group-data-[checked]:text-emerald-300',
+            teal: 'group-data-[checked]:text-teal-300',
+            cyan: 'group-data-[checked]:text-cyan-300',
+            sky: 'group-data-[checked]:text-sky-300',
+            blue: 'group-data-[checked]:text-blue-400',
+            indigo: 'group-data-[checked]:text-indigo-400',
+            violet: 'group-data-[checked]:text-violet-400',
+            purple: 'group-data-[checked]:text-purple-400',
+            fuchsia: 'group-data-[checked]:text-fuchsia-400',
+            pink: 'group-data-[checked]:text-pink-400',
+            rose: 'group-data-[checked]:text-rose-400',
+            neutral: 'group-data-[checked]:text-neutral-400',
           },
         },
       }),
       icon: cva('size-4'),
     },
   },
-  label: typographyStyles.root({ variant: 'label', className: 'px-3 pb-1' }),
+  label: typographyStyles.root({
+    variant: 'label',
+    className: 'px-3 pb-1 truncate',
+  }),
 }
 
 export default annotationRadioStyles
