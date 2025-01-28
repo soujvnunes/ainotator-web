df8ac0a52a1c8076f5185dbacc6356588d7686e9
Victor <soujvnunes@gmail.com>
2025-01-27 21:33:49 -0300

    feat(hooks/useBrush) only render during annotation

--- hooks/useBrush.ts
+++ hooks/useBrush.ts
@@ -8,16 +8,18 @@ import useCanvasRefs from './useCanvasRefs'
 export default function useBrush() {
   const annotatorRefs = useCanvasRefs()
   const size = useAppState((state) => state.annotator.current.size.brush)
+  const mode = useAppState((state) => state.annotator.current.mode)
   const category = useAppState((state) => state.annotator.current.category)
 
   useEffect(() => {
     const canvas = annotatorRefs.canvas.current
 
-    if (canvas == null || category?.type !== 'brush') return
+    if (canvas == null || mode !== 'annotating' || category?.type !== 'brush')
+      return
 
     canvas.isDrawingMode = true
     canvas.freeDrawingBrush = new PencilBrush(canvas)
     canvas.freeDrawingBrush.color = `rgb(${category.color} / 0.4)`
     canvas.freeDrawingBrush.width = size
-  }, [annotatorRefs, category, size])
+  }, [annotatorRefs, category, mode, size])
 }
