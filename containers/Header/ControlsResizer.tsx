b5eeff3e63b2b15231c53c13ef08492aca8615d8
Victor <soujvnunes@gmail.com>
2025-01-27 21:35:16 -0300

    feat(containers/ControlsResizer) implement  range field

--- containers/Header/ControlsResizer.tsx
+++ containers/Header/ControlsResizer.tsx
@@ -2,16 +2,12 @@
 
 import { useCallback } from 'react'
 
+import annotator from '@/lib/annotator'
+
 import useAppDispatch from '@/hooks/useAppDispatch'
 import useAppState from '@/hooks/useAppState'
-import { Field, Input, Label } from '@headlessui/react'
 
-function translateX(input: number) {
-  return ((160 - 0) / (40 - 10)) * (input - 10)
-}
-function translateY(input: number) {
-  return -0.5 * input + 20
-}
+import { FieldRange } from '@/ui'
 
 export default function ControlsResizer() {
   const mode = useAppState((state) => state.annotator.current.mode)
@@ -20,45 +16,17 @@ export default function ControlsResizer() {
   const dispatch = useAppDispatch()
   const handleSize = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch.annotator.setSize({ brush: +event.target.value })
+      dispatch(annotator.actions.setSize({ brush: +event.target.value }))
     },
-    [dispatch.annotator],
+    [dispatch],
   )
-  const max = 40
 
   return (
-    <Field
+    <FieldRange
+      label="Brush size"
+      value={size}
+      onChange={handleSize}
       disabled={mode !== 'annotating' || category?.type !== 'brush'}
-      className="group relative mx-5 inline-flex h-10 w-40 items-center">
-      <Label className="sr-only">Brush size</Label>
-      <Input
-        className="w-full cursor-pointer opacity-0 group-data-[disabled]:cursor-not-allowed"
-        type="range"
-        min={10}
-        max={max}
-        value={size}
-        onChange={handleSize}
-      />
-      <div
-        aria-hidden
-        className="absolute inset-0 -z-10">
-        <div
-          data-range-type="track"
-          className="absolute top-[15px] h-[10px] w-full bg-neutral-800"
-        />
-        <button
-          type="button"
-          data-range-type="thumb"
-          className="absolute left-[--left] top-[--top] h-[--size] w-[--size] bg-white group-data-[disabled]:bg-neutral-600"
-          style={
-            {
-              '--size': `${size}px`,
-              '--top': `${translateY(size)}px`,
-              '--left': `${translateX(size) - size / 2}px`,
-            } as React.CSSProperties
-          }
-        />
-      </div>
-    </Field>
+    />
   )
 }
