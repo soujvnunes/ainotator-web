0f19421a60473ba945e5274ba1e086d91ced968d
Victor <soujvnunes@gmail.com>
2025-01-29 13:03:07 -0300

    feat(hooks/useBrush) implement useCurrentCategory

--- hooks/useBrush.ts
+++ hooks/useBrush.ts
@@ -4,12 +4,13 @@ import { PencilBrush } from 'fabric'
 
 import useAppState from './useAppState'
 import useCanvasRefs from './useCanvasRefs'
+import useCurrentCategory from './useCurrentCategory'
 
 export default function useBrush() {
+  const category = useCurrentCategory()
   const annotatorRefs = useCanvasRefs()
   const size = useAppState((state) => state.annotator.current.size.brush)
   const mode = useAppState((state) => state.annotator.current.mode)
-  const category = useAppState((state) => state.annotator.current.category)
 
   useEffect(() => {
     const canvas = annotatorRefs.canvas.current
