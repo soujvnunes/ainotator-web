d429eec9e44db750dee91398353155a00264d195
Victor <soujvnunes@gmail.com>
2025-01-29 14:15:59 -0300

    feat(lib/annotator) add colors source of truth

--- lib/annotator.ts
+++ lib/annotator.ts
@@ -7,11 +7,17 @@ import { createSlice } from '@reduxjs/toolkit'
 import { type PayloadAction } from '@reduxjs/toolkit'
 
 // prettier-ignore
+export const annotatorColors = {
+  bg: { red: 'bg-red-500', orange: 'bg-orange-500', yellow: 'bg-yellow-500', green: 'bg-green-500', teal: 'bg-teal-500', sky: 'bg-sky-500', indigo: 'bg-indigo-500', purple: 'bg-purple-500', pink: 'bg-pink-500', neutral: 'bg-neutral-500' },
+  text: { red: 'group-data-[checked]:text-red-500', orange: 'group-data-[checked]:text-orange-500', yellow: 'group-data-[checked]:text-yellow-500', green: 'group-data-[checked]:text-green-500', teal: 'group-data-[checked]:text-teal-500', sky: 'group-data-[checked]:text-sky-500', indigo: 'group-data-[checked]:text-indigo-500', purple: 'group-data-[checked]:text-purple-500', pink: 'group-data-[checked]:text-pink-500', neutral: 'group-data-[checked]:text-neutral-500' },
+  channel: { red: '239 68 68', orange: '249 115 22', yellow: '132 204 22', green: '34 197 94', teal: '20 184 166', sky: '14 165 233', indigo: '99 102 241', purple: '168 85 247', pink: '236 72 153', neutral: '115 115 115' },
+} as const
+// prettier-ignore
 export const annotatorCategoryColors = ['red', 'orange', 'yellow', 'green', 'teal', 'sky', 'indigo', 'purple', 'pink', 'neutral'] as const
-export const annotatorCategoryType = ['brush', 'polygon'] as const
+export const annotatorCategoryTypes = ['brush', 'polygon'] as const
 export const annotatorCategoryCrowds = ['yes', 'no'] as const
 
-export type AnnotatorCategoryTypes = (typeof annotatorCategoryType)[number]
+export type AnnotatorCategoryTypes = (typeof annotatorCategoryTypes)[number]
 export type AnnotatorCategoryCrowds = (typeof annotatorCategoryCrowds)[number]
 export type AnnotatorCategoryColors = (typeof annotatorCategoryColors)[number]
 
