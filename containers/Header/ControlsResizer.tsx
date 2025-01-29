8753c96a969dc5a9fabdcac5bbca696818b9a0e6
Victor <soujvnunes@gmail.com>
2025-01-29 13:03:44 -0300

    feat(containers/ControlsResizer) implement useCurrentCategory

--- containers/Header/ControlsResizer.tsx
+++ containers/Header/ControlsResizer.tsx
@@ -6,12 +6,13 @@ import annotator from '@/lib/annotator'
 
 import useAppDispatch from '@/hooks/useAppDispatch'
 import useAppState from '@/hooks/useAppState'
+import useCurrentCategory from '@/hooks/useCurrentCategory'
 
 import { RangeField } from '@/ui'
 
 export default function ControlsResizer() {
+  const category = useCurrentCategory()
   const mode = useAppState((state) => state.annotator.current.mode)
-  const category = useAppState((state) => state.annotator.current.category)
   const size = useAppState((state) => state.annotator.current.size.brush)
   const dispatch = useAppDispatch()
   const handleSize = useCallback(
