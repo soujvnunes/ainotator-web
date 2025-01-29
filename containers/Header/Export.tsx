60936d216258bbfda1eb39640d2800fd0e493513
Victor <soujvnunes@gmail.com>
2025-01-29 19:48:17 -0300

    feat(containers/Header) implement license and info forms

--- containers/Header/Export.tsx
+++ containers/Header/Export.tsx
@@ -1,34 +1,54 @@
 'use client'
 
-import { TabGroup } from '@headlessui/react'
+import { useCallback } from 'react'
+
+import { TabGroup, TabPanel, TabPanels } from '@headlessui/react'
 import { DocumentArrowDownIcon } from '@heroicons/react/24/solid'
 
+import annotator from '@/lib/annotator'
 import { tabs } from '@/lib/exportForm'
 
+import useAppDispatch from '@/hooks/useAppDispatch'
 import useAppState from '@/hooks/useAppState'
 
 import { Dialog, IconButton, Tabs } from '@/ui'
 
-import ExportForm from './ExportForm'
+import ExportInfo from './ExportInfo'
+import ExportLicense from './ExportLicense'
 
 export default function Export() {
+  const dispatch = useAppDispatch()
   const annotations = useAppState((state) => state.dataset.annotations)
+  const renderController = useCallback(
+    (open: () => void) => (
+      <IconButton
+        onClick={() => {
+          open()
+          dispatch(annotator.actions.setMode('exporting'))
+        }}
+        disabled={!annotations.length}
+        aria-label="Export annotations in COCO format">
+        <DocumentArrowDownIcon className="m-auto size-6" />
+      </IconButton>
+    ),
+    [dispatch, annotations.length],
+  )
 
   return (
     <Dialog
       title="Dataset Details"
       description="Fill in the license and information dataset details to validate it before exporting."
-      renderController={(open) => (
-        <IconButton
-          onClick={open}
-          disabled={!annotations.length}
-          aria-label="Export annotations in COCO format">
-          <DocumentArrowDownIcon className="m-auto size-6" />
-        </IconButton>
-      )}>
+      renderController={renderController}>
       <TabGroup>
         <Tabs value={tabs} />
-        <ExportForm />
+        <TabPanels>
+          <TabPanel>
+            <ExportInfo />
+          </TabPanel>
+          <TabPanel>
+            <ExportLicense />
+          </TabPanel>
+        </TabPanels>
       </TabGroup>
     </Dialog>
   )
