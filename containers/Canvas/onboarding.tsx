268553a65eac813e2cf0c3affd2edd9ca4b1ef43
Victor <soujvnunes@gmail.com>
2025-01-31 14:18:01 -0300

    feat(container/canvas) rename uploader onboarding to onboarding

--- containers/Canvas/onboarding.tsx
+++ containers/Canvas/onboarding.tsx
@@ -1,15 +1,20 @@
-import {
-  DocumentArrowDownIcon,
-  DocumentArrowUpIcon,
-  PaintBrushIcon,
-  PlusIcon,
-} from '@heroicons/react/24/solid'
+import { classNames } from '@/helpers'
+
+import { useStoreState } from '@/hooks'
 
 import { typographyStyles } from '@/ui'
 
-export default function UploaderOnboarding() {
+import onboardingItems from './onboarding-items'
+
+const rootProps = classNames(
+  'relative flex h-full w-full flex-col items-center justify-center aria-hidden:hidden',
+)
+
+export default function Onboarding() {
+  const mode = useStoreState((state) => state.annotator.mode)
+
   return (
-    <span className="m-auto flex flex-col items-center">
+    <section {...rootProps({ aria: { hidden: mode !== 'waiting' } })}>
       <p
         className={typographyStyles.root({
           variant: 'label',
@@ -21,7 +26,7 @@ export default function UploaderOnboarding() {
         Start by adding <br /> an image
       </h2>
       <ul className="inline-flex flex-col items-center text-white/60">
-        {items.map(({ Icon, label }) => (
+        {onboardingItems.map(({ Icon, label }) => (
           <li
             key={label}
             className="flex items-center">
@@ -34,13 +39,7 @@ export default function UploaderOnboarding() {
           </li>
         ))}
       </ul>
-    </span>
+      <div className="absolute inset-0 bg-[url(/rapport.png)] from-black/20 to-black/0 bg-repeat [mask-image:radial-gradient(circle_at_center,_var(--tw-gradient-stops))]" />
+    </section>
   )
 }
-
-const items = [
-  { Icon: DocumentArrowUpIcon, label: 'Pick your image' },
-  { Icon: PlusIcon, label: 'Add classes names' },
-  { Icon: PaintBrushIcon, label: 'Annotate your image' },
-  { Icon: DocumentArrowDownIcon, label: 'Export in COCO format' },
-]
