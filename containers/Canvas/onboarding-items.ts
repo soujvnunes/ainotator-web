975c9830ff13f0f1aa1497139820c7db31c16fea
Victor <soujvnunes@gmail.com>
2025-01-31 14:18:08 -0300

    feat(container/canvas) extract onboarding items

---
+++ containers/Canvas/onboarding-items.ts
@@ -0,0 +1,15 @@
+import {
+  DocumentArrowDownIcon,
+  DocumentArrowUpIcon,
+  PaintBrushIcon,
+  PlusIcon,
+} from '@heroicons/react/24/solid'
+
+const onboardingItems = [
+  { Icon: DocumentArrowUpIcon, label: 'Pick your image' },
+  { Icon: PlusIcon, label: 'Add classes names' },
+  { Icon: PaintBrushIcon, label: 'Annotate your image' },
+  { Icon: DocumentArrowDownIcon, label: 'Export in COCO format' },
+]
+
+export default onboardingItems
