6c4e4ed896af893b568c2e0a4dc68ef57e1ee898
Victor <soujvnunes@gmail.com>
2025-01-30 22:17:58 -0300

    feat(prettier.config) list cx group

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['tv', 'cva'],
+  tailwindFunctions: ['tv', 'cva', 'cx'],
   jsxSingleQuote: false,
   bracketSameLine: true,
   singleAttributePerLine: true,
