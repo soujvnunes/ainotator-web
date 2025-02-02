91aabdd6952c74165911d152f5c676f6cd0da9db
Victor <soujvnunes@gmail.com>
2025-02-02 18:23:19 -0300

    feat(ui/*) apply tailwind typography classes

--- ui/Button/styles.ts
+++ ui/Button/styles.ts
@@ -1,20 +1,14 @@
 import { cva } from 'class-variance-authority'
 
-import { typographyStyles } from '../typography'
-
 const buttonStyles = {
   root: cva(
-    typographyStyles.root({
-      variant: 'label',
-      className:
-        'inline-flex h-10 min-w-10 flex-shrink-0 cursor-pointer items-center justify-center px-4 data-[disabled]:cursor-not-allowed data-[focus]:outline-none',
-    }),
+    'text-label inline-flex h-10 min-w-10 shrink-0 cursor-pointer items-center justify-center px-4 data-disabled:cursor-not-allowed data-focus:outline-hidden',
     {
       variants: {
         variant: {
           filled:
-            'bg-white text-black data-[disabled]:bg-white/60 data-[hover]:bg-white/60 data-[disabled]:text-black/60',
-          text: 'text-white data-[hover]:bg-white/20 data-[disabled]:text-white/40',
+            'bg-white text-black data-disabled:bg-white/60 data-hover:bg-white/60 data-disabled:text-black/60',
+          text: 'text-white data-hover:bg-white/20 data-disabled:text-white/40',
         },
         fullWidth: { true: 'w-full', false: '' },
       },
