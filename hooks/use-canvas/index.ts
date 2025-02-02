43025c7a21882621031cc926318dc944ed651a89
Victor <soujvnunes@gmail.com>
2025-02-02 12:54:06 -0300

    feat(hooks/use-canvas) simplify annotator refs provider

--- hooks/use-canvas/index.ts
+++ hooks/use-canvas/index.ts
@@ -1,31 +1,9 @@
 'use client'
 
-import { useEffect, useId } from 'react'
+import { useContext } from 'react'
 
-import { Canvas } from 'fabric'
+import { CanvasContext } from '@/lib'
 
-import useRefs from '../use-refs'
-
-interface CanvasArgs {
-  offset: {
-    y: number
-  }
-}
-
-export default function useCanvas(props: CanvasArgs) {
-  const annotatorRefs = useRefs()
-  const id = useId()
-
-  useEffect(() => {
-    annotatorRefs.canvas.current = new Canvas(id, {
-      width: window.innerWidth,
-      height: window.innerHeight - props.offset.y,
-    })
-
-    return () => {
-      annotatorRefs.canvas.current?.dispose()
-    }
-  }, [annotatorRefs.canvas, id, props.offset.y])
-
-  return id
+export default function useCanvas() {
+  return useContext(CanvasContext)
 }
