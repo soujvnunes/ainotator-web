99c4f22f8e73123edf4bd14e14f54db0c20cd002
Victor <soujvnunes@gmail.com>
2025-02-02 13:01:58 -0300

    feat(helpers/classes-variants) implement variants getter on variants

--- helpers/classes-variants/index.ts
+++ helpers/classes-variants/index.ts
@@ -1,19 +1,12 @@
 import { twMerge } from 'tailwind-merge'
 
-import prefixSet, { type BooleanObject } from './prefix-set'
+import getVariants, { type Variant } from './get-variants'
 
-type Set<D extends string, A extends string> =
-  | { data: BooleanObject<D>; aria?: BooleanObject<A> }
-  | { data?: BooleanObject<D>; aria: BooleanObject<A> }
-
-export default function classes<D extends string, A extends string>(
-  ...classNames: string[]
-) {
-  return function variants(set: Set<D, A>) {
+export default function classes(...classNames: string[]) {
+  return function variants<K extends string>(variant: Variant<K>) {
     return {
       className: twMerge(classNames),
-      ...prefixSet('data', set.data),
-      ...prefixSet('aria', set.aria),
+      ...getVariants(variant),
     }
   }
 }
