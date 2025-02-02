39baea202384b95dde5a8776088b8f2b28dec310
Victor <soujvnunes@gmail.com>
2025-02-02 18:20:16 -0300

    feat(containers/canvas) apply tailwind typography classes

--- containers/Canvas/onboarding.tsx
+++ containers/Canvas/onboarding.tsx
@@ -4,8 +4,6 @@ import { classes } from '@/helpers'
 
 import { useStoreState } from '@/hooks'
 
-import { typographyStyles } from '@/ui'
-
 import OnboardingAddFile from './onboarding-add-file'
 import onboardingItems from './onboarding-items'
 
@@ -14,17 +12,11 @@ export default function Onboarding() {
 
   return (
     <section {...rootVariants({ inert: mode !== 'waiting' })}>
-      <p
-        className={typographyStyles.root({
-          variant: 'label',
-          className: 'text-white/60',
-        })}>
-        AINotator WEB
-      </p>
-      <h2 className="mb-4 text-4xl text-center lg:text-6xl">
+      <p className="text-label text-white/60">AINotator WEB</p>
+      <h2 className="mb-4 text-center text-4xl lg:text-6xl">
         Start by adding <br /> an image
       </h2>
-      <ul className="z-10 inline-flex flex-col items-center text-white/60">
+      <ul className="inline-flex flex-col items-center text-white/60">
         <li className="flex items-center">
           <OnboardingAddFile />
         </li>
@@ -33,7 +25,7 @@ export default function Onboarding() {
             key={label}
             className="flex items-center">
             <span
-              className="flex w-12 h-12 mr-4 text-black bg-white"
+              className="mr-4 flex h-12 w-12 bg-white text-black"
               aria-label={label}>
               <Icon className="m-auto size-6" />
             </span>
@@ -41,16 +33,11 @@ export default function Onboarding() {
           </li>
         ))}
       </ul>
-      <div className="absolute inset-0 bg-[url(/rapport.png)] from-black/20 to-black/0 bg-repeat [mask-image:radial-gradient(circle_at_center,_var(--tw-gradient-stops))]" />
     </section>
   )
 }
 
 const rootVariants = classes(
-  'absolute flex h-full w-full flex-col items-center justify-center',
-  /**
-   * TODO: remove when update to tailwind 4
-   * https://tailwindcss.com/docs/hover-focus-and-other-states#styling-inert-elements
-   */
-  'data-[inert]:hidden',
+  'absolute z-10 flex h-full w-full flex-col items-center justify-center',
+  'inert:hidden',
 )
