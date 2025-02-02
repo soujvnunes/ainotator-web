a823a93c10746fe1ed19681efc0e756e6cf1a4ac
Victor <soujvnunes@gmail.com>
2025-02-01 23:07:45 -0300

    feat(ui/radio-field) add vertical prop

--- ui/radio-field/styles.ts
+++ ui/radio-field/styles.ts
@@ -1,15 +1,44 @@
 import { cva } from 'class-variance-authority'
 
 import { buttonStyles } from '../button'
+import { iconButtonStyles } from '../icon-button'
 
 const radioFieldStyles = {
-  root: cva('group flex'),
-  radio: buttonStyles.root({
-    variant: 'text',
-    fullWidth: true,
-    className:
-      'data-[checked]:data-[hover]:bg-white/60 data-[checked]:data-[disabled]:bg-white/40 data-[checked]:bg-white shrink data-[checked]:text-black data-[checked]:data-[disabled]:text-black/60',
+  root: cva('group flex', {
+    variants: {
+      vertical: {
+        true: 'flex-col',
+        false: '',
+      },
+    },
   }),
+  radio: {
+    root: cva(
+      buttonStyles.root({
+        variant: 'text',
+        className: 'bg-neutral-800 grow',
+      }),
+      {
+        variants: {
+          vertical: {
+            true: 'group pl-0 justify-start',
+            false:
+              'data-[checked]:data-[hover]:bg-white/60 data-[checked]:data-[disabled]:bg-white/40 data-[checked]:bg-white data-[checked]:text-black data-[checked]:data-[disabled]:text-black/80',
+          },
+        },
+        defaultVariants: {
+          vertical: false,
+        },
+      },
+    ),
+    button: {
+      root: iconButtonStyles.root({
+        className:
+          'inline-flex mr-4 group-data-[checked]:group-data-[hover]:bg-white/60 group-data-[checked]:group-data-[disabled]:bg-white/40 group-data-[checked]:bg-white group-data-[checked]:text-black group-data-[checked]:group-data-[disabled]:text-black/60',
+      }),
+      icon: 'invisible m-auto group-data-[checked]:visible size-6',
+    },
+  },
 }
 
 export default radioFieldStyles
