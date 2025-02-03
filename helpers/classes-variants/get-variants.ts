192c287e5eda5fd84952f9ca05b5d21b9553be07
Victor <soujvnunes@gmail.com>
2025-02-02 21:51:03 -0300

    feat(helpers/classes-variants) allow pass value of string type besides boolean

--- helpers/classes-variants/get-variants.ts
+++ helpers/classes-variants/get-variants.ts
@@ -3,33 +3,38 @@ import { type AttributeVariants } from './attribute-variants'
 import isAriaVariant from './is-aria-variant'
 import isAttributeVariant from './is-attribute-variant'
 
-export type Variant<V extends string> = Partial<
-  Record<V | AriaVariants | AttributeVariants, boolean>
+type Value = string | boolean
+
+export type Variant = Partial<
+  Record<AriaVariants | AttributeVariants, Value> & Record<string & {}, Value>
 >
-export type Property<V extends string> = V extends AriaVariants
-  ? `aria-${V}`
-  : V extends AttributeVariants
-    ? V
-    : `data-${V}`
 
-export default function getVariants<V extends string>(
-  variant: Variant<V>,
-): Record<Property<V>, boolean | undefined> {
-  const attributes: Record<string, boolean | undefined> = {}
+export type Attribute<K extends string> = K extends AriaVariants
+  ? `aria-${K}`
+  : K extends AttributeVariants
+    ? K
+    : `data-${K}`
 
-  for (const [key, value] of Object.entries(variant) as [V, boolean][]) {
-    let prop: string
+export type Attributes<V extends Variant> = {
+  [K in keyof V as Attribute<K & string>]: V[K] | undefined
+}
+
+export default function getVariants<V extends Variant>(
+  variant: V,
+): Attributes<V> {
+  const attributes: Record<string, Value | undefined> = {}
+
+  for (const [key, value] of Object.entries(variant)) {
+    let prop = `data-${key}`
 
     if (isAriaVariant(key)) {
       prop = `aria-${key}`
     } else if (isAttributeVariant(key)) {
       prop = key
-    } else {
-      prop = `data-${key}`
     }
 
     attributes[prop] = value || undefined
   }
 
-  return attributes
+  return attributes as Attributes<V>
 }
