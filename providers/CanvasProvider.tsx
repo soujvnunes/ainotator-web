bfaa7739c2f3683a2735b14a01c6044db7eea65d
Victor <soujvnunes@gmail.com>
2025-02-11 18:57:21 -0300

    feat(providers) remove lib barrel file

--- providers/CanvasProvider.tsx
+++ providers/CanvasProvider.tsx
@@ -4,7 +4,7 @@ import { useRef } from 'react'
 
 import type { Canvas } from 'fabric'
 
-import { CanvasContext } from '@/lib'
+import CanvasContext from '@/lib/CanvasContext'
 
 export default function CanvasProvider(props: {
   readonly children: React.ReactNode
--- providers/StoreProvider.tsx
+++ providers/StoreProvider.tsx
@@ -2,7 +2,7 @@
 
 import { Provider } from 'react-redux'
 
-import { store } from '@/lib'
+import store from '@/lib/store'
 
 export default function StoreProvider(props: {
   readonly children: React.ReactNode
