d3e4e81fa70579960350cd153717f81fba095955
Victor <soujvnunes@gmail.com>
2025-02-12 19:23:37 -0300

    feat(hooks/useBrush) implement brush width

--- hooks/useBrush.ts
+++ hooks/useBrush.ts
@@ -2,7 +2,9 @@ import { useEffect } from 'react'
 
 import { PencilBrush } from 'fabric'
 
+import selectBrushWidth from '@/selectors/selectBrushWidth'
 import selectCurrentCategory from '@/selectors/selectCurrentCategory'
+import selectIsAnnotating from '@/selectors/selectIsAnnoting'
 
 import useCanvas from './useCanvas'
 import useStoreState from './useStoreState'
@@ -10,17 +12,21 @@ import useStoreState from './useStoreState'
 export default function useBrush() {
   const canvas = useCanvas()
   const category = useStoreState(selectCurrentCategory)
-  const size = useStoreState((state) => state.annotator.size.brush)
-  const mode = useStoreState((state) => state.annotator.mode)
+  const brushWidth = useStoreState(selectBrushWidth)
+  const isAnnotating = useStoreState(selectIsAnnotating)
 
   useEffect(() => {
     const _canvas = canvas.current
 
-    if (!_canvas || mode !== 'annotating' || category?.type !== 'brush') return
+    if (!_canvas) return
+
+    _canvas.isDrawingMode = false
+
+    if (!isAnnotating || category?.type !== 'brush') return
 
     _canvas.isDrawingMode = true
     _canvas.freeDrawingBrush = new PencilBrush(_canvas)
     _canvas.freeDrawingBrush.color = category.color
-    _canvas.freeDrawingBrush.width = size
-  }, [canvas, category, mode, size])
+    _canvas.freeDrawingBrush.width = brushWidth
+  }, [canvas, category, isAnnotating, brushWidth])
 }
