0cc10fee6e1e11f47f7d8505fa1362c3514d49f0
Victor <soujvnunes@gmail.com>
2025-02-12 18:23:49 -0300

    feat(containers/Toolbar) implement category selector

--- containers/Toolbar/ControlsResizer.tsx
+++ containers/Toolbar/ControlsResizer.tsx
@@ -4,14 +4,15 @@ import { useCallback } from 'react'
 
 import annotator from '@/reducers/annotator'
 
-import useCurrentCategory from '@/hooks/useCurrentCategory'
+import selectCategory from '@/selectors/selectCategory'
+
 import useStoreDispatch from '@/hooks/useDispatch'
 import useStoreState from '@/hooks/useStoreState'
 
 import RangeField from '@/components/RangeField'
 
 export default function ControlsResizer() {
-  const category = useCurrentCategory()
+  const category = useStoreState(selectCategory)
   const mode = useStoreState((state) => state.annotator.mode)
   const size = useStoreState((state) => state.annotator.size.brush)
   const dispatch = useStoreDispatch()
