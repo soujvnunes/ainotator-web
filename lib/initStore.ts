b6d8aa12c1373b5a1f10bc460838a90de682573f
Victor <soujvnunes@gmail.com>
2025-04-18 20:35:21 -0300

    lib/initStore: implement datasetApi reducer and middleware

--- lib/initStore.ts
+++ lib/initStore.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import annotatorSlice from '@/slices/annotatorSlice'
-import datasetSlice from '@/slices/datasetSlice'
+import datasetSlice, { datasetApi } from '@/slices/datasetSlice'
 
 export default function initStore() {
   return configureStore({
     reducer: {
       [datasetSlice.reducerPath]: datasetSlice.reducer,
       [annotatorSlice.reducerPath]: annotatorSlice.reducer,
+      [datasetApi.reducerPath]: datasetApi.reducer,
+    },
+    middleware(getDefaultMiddleware) {
+      return getDefaultMiddleware().concat(datasetApi.middleware)
     },
   })
 }
