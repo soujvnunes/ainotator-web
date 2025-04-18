16a9728081eabb902352cf0e0d470d17de24969d
Victor <soujvnunes@gmail.com>
2025-04-18 20:38:56 -0300

    containers/Header/ActionsExportDownload: implement validation when mount

---
+++ containers/Header/ActionsExportDownload.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useCallback, useEffect } from 'react'
+
+import { datasetApi } from '@/slices/datasetSlice'
+
+import selectDataset from '@/selectors/selectDataset'
+
+import generateLink from '@/helpers/generateLink'
+
+import useCanvas from '@/hooks/useCanvas'
+import useStoreState from '@/hooks/useStoreState'
+
+import Button from '@/components/Button'
+
+export default function ActionsExportDownload() {
+  const canvas = useCanvas()
+
+  const dataset = useStoreState(selectDataset)
+
+  const [validate, validation] = datasetApi.useValidateMutation()
+
+  const handleDownload = useCallback(() => {
+    canvas.current?.clear()
+    generateLink({
+      name: `${dataset.info.date_created}_annotations.json`,
+      value: dataset,
+    })
+  }, [canvas, dataset])
+
+  useEffect(() => {
+    validate(dataset)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  console.log(validation)
+
+  return (
+    // TODO: enhance UI
+    <>
+      {validation.isLoading && 'Validating...'}
+      {/** TODO: format */}
+      {validation.data?.errors?.map((error) => (
+        <p key={error.message}>
+          {error.instancePath} {error.message}
+        </p>
+      ))}
+      <Button
+        type="submit"
+        disabled={validation.isLoading || !validation.data?.isValid}
+        onClick={handleDownload}
+        fullWidth>
+        Download
+      </Button>
+    </>
+  )
+}
