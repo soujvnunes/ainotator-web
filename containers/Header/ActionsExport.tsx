291019dd60994d3e160c7a2e6ce98b205ba5f47c
Victor <soujvnunes@gmail.com>
2025-04-18 20:39:21 -0300

    containers/Header/ActionsExport: implement view and dataset validation in a dialog component

--- containers/Header/ActionsExport.tsx
+++ containers/Header/ActionsExport.tsx
@@ -1,43 +1,45 @@
 'use client'
 
-import { useCallback } from 'react'
+import { Suspense } from 'react'
 
 import { DocumentArrowDownIcon } from '@heroicons/react/24/solid'
 
 import datasetSlice from '@/slices/datasetSlice'
 
-import selectCurrentImage from '@/selectors/selectCurrentImage'
-
-import generateLink from '@/helpers/generateLink'
-
-import useCanvas from '@/hooks/useCanvas'
 import useStoreState from '@/hooks/useStoreState'
 
+import Dialog from '@/components/Dialog'
 import IconButton from '@/components/IconButton'
 
+import ActionsExportDownload from './ActionsExportDownload'
+import ActionsExportViewer from './ActionsExportViewer'
+
 export default function ActionsExport() {
-  const canvas = useCanvas()
-  const info = useStoreState(datasetSlice.selectors.info)
   const annotations = useStoreState(datasetSlice.selectors.annotations)
-  const image = useStoreState(selectCurrentImage)
-  const handleValidation = useCallback(() => {
-    const _canvas = canvas.current
-
-    if (!_canvas || !image) return
-
-    generateLink({
-      name: `${image.file_name}_${info.date_created}_annotations.json`,
-      value: { info, annotations },
-    })
-    _canvas.clear()
-  }, [annotations, canvas, image, info])
 
   return (
-    <IconButton
-      onClick={handleValidation}
-      disabled={!annotations.length}
-      aria-label="Export annotations in COCO format">
-      <DocumentArrowDownIcon className="m-auto size-6" />
-    </IconButton>
+    <Dialog
+      size="lg"
+      title="Validate dataset"
+      description="View the provided dataset details before exporting."
+      renderController={(open) => (
+        <IconButton
+          onClick={open}
+          disabled={!annotations.length}
+          aria-label="Export annotations in COCO format">
+          <DocumentArrowDownIcon className="m-auto size-6" />
+        </IconButton>
+      )}>
+      <div className="grid grid-cols-2">
+        <div className="h-full max-h-96 overflow-y-auto">
+          <Suspense fallback="Loading...">
+            <ActionsExportViewer />
+          </Suspense>
+        </div>
+        <div className="p-4">
+          <ActionsExportDownload />
+        </div>
+      </div>
+    </Dialog>
   )
 }
