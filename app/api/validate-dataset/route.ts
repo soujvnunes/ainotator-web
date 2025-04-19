2d0f3c978b93810752fd608533dfe022d1192686
Victor <soujvnunes@gmail.com>
2025-04-19 15:13:19 -0300

    app/api/validate-dataset: wrap within /api

--- app/api/validate-dataset/route.ts
+++ app/api/validate-dataset/route.ts
@@ -1,13 +1,13 @@
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 
-import schema from './schema'
+import datasetSchema from '@/consts/datasetSchema'
 
 const ajv = new Ajv({ allErrors: true })
 
 addFormats(ajv)
 
-const validate = ajv.compile(schema)
+const validate = ajv.compile(datasetSchema)
 
 export async function POST(req: Request) {
   const dataset = await req.json()
